d6171ace7406a2319eb48ffa274e137debc6fbc1
feyyzanur <112513981+feyyzanur@users.noreply.github.com>
2022-09-06 22:47:58 +0300

    Update ödev2.sql

--- ödev2.sql
+++ ödev2.sql
@@ -2,7 +2,7 @@
 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız.(BETWEEN - AND yapısını kullanınız.)*/
 
 SELECT * FROM film
-WHERE replacement_cost BETWEEN 12.99 AND 16.99 ;
+WHERE (replacement_cost BETWEEN 12.99 AND 16.99) AND replacement_cost <> 16.99 ;
 
 /* Actor tablosunda bulunan first_name ve last_name 
 sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız.(IN operatörünü kullanınız.)*/
